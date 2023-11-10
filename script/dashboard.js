a1c30cc82dc99d77826573f24b60620b204f7bdc
Augusto Gutierrez <augustogutierrez288@gmail.com>
2023-11-10 17:41:39 -0300

    CRUD de estudiantes terminado

--- script/dashboard.js
+++ script/dashboard.js
@@ -1,21 +1,19 @@
 const nameUser = document.getElementById("input-name");
 const surnameUser = document.getElementById("input-surname");
-const commission = document.getElementById("commission");
+const careersSelect = document.getElementById("careers");
+const shiftsSelect = document.getElementById("shifts");
+const commissionSelect = document.getElementById("commission");
 const addStudent = document.getElementById("btn-add-student");
 const studentTable = document.getElementById("student-table");
 const boxSidebar = document.getElementById("box-sidebar");
 const btnSidebarToggle = document.getElementById("btn-sidebars-toggle");
+
 let formStudent;
 let btnModifyForm;
+
 let clickCount = 0;
 let ID = 0;
 let arrayStudent = [];
-
-
-btnSidebarToggle.addEventListener("click", () => {
-    boxSidebar.classList.toggle("open");
-})
-
 
 function notify(message, color) {
     Toastify({
@@ -32,20 +30,81 @@ function notify(message, color) {
       },
     }).showToast();
 }
-document.addEventListener("DOMContentLoaded", () => {
-    notify("Bienvenido", "#662d91");
-});
+function paintCareers(){
+    careersSelect.innerHTML = "";
+    careersSelect.innerHTML = `
+        <option selected>Carrera</option>
+        <option value="TUMI">Tecnicatura universitaria en mantenimiento industrial</option>
+        <option value="TUHS">Tecnicatura universitaria en higiene y seguridad</option>
+        <option value="TUES">Tecnicatura universitaria en energia sustentable</option>
+        <option value="TUP">Tecnicatura universitaria en programacion</option>
+        <option value="TUM">Tecnicatura universitaria en mecatronica</option>
+        <option value="TUL">Tecnicatura universitaria en logistica</option>
+    `;
+}
+
+function shiftsLogic(){
+    shiftsSelect.addEventListener("change", () => {
+        const shifts = shiftsSelect.value;
+        if (shifts === "Mañana") {
+            commissionSelect.innerHTML = `
+            <option selected>Comision</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            `;
+        } else if (shifts === "Tarde") {
+            commissionSelect.innerHTML = `
+            <option selected>Comision</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            `;
+        } else if (shifts === "Noche") {
+            commissionSelect.innerHTML = `
+            <option selected>Comision</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            `;
+        }else {
+            commissionSelect.innerHTML = `
+            <option selected>Comision</option>
+            `;
+        }
+    });
+}
+
+function careersLogic(){
+    careersSelect.addEventListener("change", () => {
+        if (careersSelect.value === "TUMI" || careersSelect.value === "TUHS" || careersSelect.value === "TUES" || careersSelect.value === "TUP" || careersSelect.value === "TUM" || careersSelect.value === "TUL") {
+          shiftsSelect.disabled = false;
+          commissionSelect.disabled = false;
+        } else {
+          shiftsSelect.disabled = true;
+          commissionSelect.disabled = true;
+        }
+    });
+      
+   shiftsLogic();
+}
+
+paintCareers();
+careersLogic();
 
 function paintElement(array){
     studentTable.innerHTML = "";
+    console.table(array);
     array.forEach((student) => {
         const ctStudent = document.createElement("div");
         ctStudent.classList.add("student");
         ctStudent.innerHTML = 
                                 `
-                                <div class="d-flex justify-content-center align-items-center gap-3 m-1" id="${student.id}"> 
+                                <div id="${student.id}"> 
                                 <p id="name${student.id}"> Nombre: ${student.name} </p>
                                 <p id="surname${student.id}"> Apellido: ${student.surname} </p>
+                                <p id="careers-student${student.id}">Carrera: ${student.careers}</p>
+                                <p id="shifts${student.id}">Turno: ${student.shifts}</p>
                                 <p id="commission${student.id}">Comision: ${student.commission}</p>
                                 <button class="btn btn-success" id="modify${student.id}">Modificar</button>
                                 <button class="btn btn-danger" id="delete${student.id}">Eliminar</button>
@@ -66,29 +125,6 @@ function paintElement(array){
     })
 }
 
-
-addStudent.addEventListener("click", () => {
-    
-    const objectStudent = {
-        id: ID,
-        name: nameUser.value,
-        surname: surnameUser.value,
-        commission: commission.value,
-        teacher: "----"
-    }
-
-    arrayStudent.push(objectStudent);
-    console.table(arrayStudent);
-    nameUser.value = "";
-    surnameUser.value = "";
-    commission.value = "";
-    ID = ID + 1;
-
-    paintElement(arrayStudent);
-
-    
-})
-
 function modifyStudent(arrayStudent, studentId) {
     
     if(clickCount === 0){
@@ -100,10 +136,14 @@ function modifyStudent(arrayStudent, studentId) {
         btnModifyForm.classList.add("btn", "btn-success");
         btnModifyForm.textContent = "Modificar";
         formStudent.appendChild(btnModifyForm);
+        addStudent.classList.add("close");
         clickCount++;  
     }
-    
 
+    commissionSelect.innerHTML = `
+        <option selected>Comision</option>
+    `;
+    shiftsLogic()
     const student =  arrayStudent.find(e => e.id === studentId);
     const studentIndex = arrayStudent.findIndex((student) => student.id === studentId);
 
@@ -111,7 +151,6 @@ function modifyStudent(arrayStudent, studentId) {
 
         nameUser.value = student.name;
         surnameUser.value = student.surname;
-        commission.value = student.commission;
 
         btnModifyForm.addEventListener("click", (e) =>{
             e.preventDefault();
@@ -120,26 +159,32 @@ function modifyStudent(arrayStudent, studentId) {
                 id: student.id,
                 name: nameUser.value,
                 surname: surnameUser.value,
-                commission:commission.value
+                careers: careersSelect.value,
+                shifts: shiftsSelect.value,
+                commission: commissionSelect.value,
             }
-            
+
             arrayStudent[studentIndex] = {
                 ...arrayStudent[studentIndex],
                 ...newStudent,}
-            
+
             nameUser.value = "";
             surnameUser.value = "";
-            commission.value = "";
+            commissionSelect.innerHTML = `
+            <option selected>Comision</option>
+            `;
+            shiftsSelect.disabled = true;
+            commissionSelect.disabled = true;
             paintElement(arrayStudent);
+            addStudent.classList.remove("close");
             formStudent.removeChild(btnModifyForm);
             clickCount = 0;
+            paintCareers();
         })
     }else {
       throw new Error(`El estudiante con el ID ${studentId} no existe`);
     }
   }
-  
-
 
 function deleteStudent(arrayStudent, studentId) {
     const studentIndex = arrayStudent.findIndex((student) => student.id === studentId);
@@ -148,8 +193,43 @@ function deleteStudent(arrayStudent, studentId) {
         arrayStudent.splice(studentIndex, 1);
         console.table(arrayStudent);
         paintElement(arrayStudent);
+        paintCareers();
     } else {
         throw new Error(`El estudiante con el ID ${studentId} no existe`);
     }
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+    notify("Bienvenido", "#662d91");
+});
+
+btnSidebarToggle.addEventListener("click", () => {
+    boxSidebar.classList.toggle("open");
+})
+
+addStudent.addEventListener("click", () => {
+    
+    const objectStudent = {
+        id: ID,
+        name: nameUser.value,
+        surname: surnameUser.value,
+        careers: careersSelect.value,
+        shifts: shiftsSelect.value,
+        commission: commission.value,
+    }
+
+    arrayStudent.push(objectStudent);
+    nameUser.value = "";
+    surnameUser.value = "";
+    paintCareers();
+    commissionSelect.innerHTML = `
+    <option selected>Comision</option>
+    `;
+    shiftsSelect.disabled = true;
+    commissionSelect.disabled = true;
+    ID = ID + 1;
+    shiftsLogic()
+    paintElement(arrayStudent);
+
+    
+})
